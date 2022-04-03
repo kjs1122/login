0cd13f1802aa669cb33064da790be3dadf69a793
kimjisoo <kimjisoo3653@nate.com>
2022-04-03 21:46:18 +0900

    login session

---
+++ src/main/java/hello/login/web/login/LoginForm.java
@@ -0,0 +1,15 @@
+package hello.login.web.login;
+
+import lombok.Data;
+
+import javax.validation.constraints.NotEmpty;
+
+@Data
+public class LoginForm {
+
+    @NotEmpty
+    public String loginId;
+
+    @NotEmpty
+    public String password;
+}
